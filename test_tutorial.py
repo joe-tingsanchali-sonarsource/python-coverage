f8234fb9a791d46052ff2f6dcb52bcc5adfe46d1
doctorsmonsters <54565237+doctorsmonsters@users.noreply.github.com>
2020-08-04 09:35:37 -0400

    Added test for branch

--- test_tutorial.py
+++ test_tutorial.py
@@ -13,3 +13,11 @@ class PrintingTest(TestCase):
         with patch('sys.stdout', new=StringIO()) as fake_out:
             say_hello(name)
             self.assertEqual(fake_out.getvalue(), expected_output)
+
+    def test_say_hello_noname(self):
+        name = ''
+        expected_output = 'Hello Stranger\n'
+
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            say_hello(name)
+            self.assertEqual(fake_out.getvalue(), expected_output)
